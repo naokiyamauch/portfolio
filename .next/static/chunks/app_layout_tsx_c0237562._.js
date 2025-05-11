4b40b3d4478fd1c4f5fd664841182fc2fed86af9
Naoki Yamauchi <165691267+naokiyamauch@users.noreply.github.com>
2025-05-11 09:23:55 -0700

    fix: remove .next from git tracking

--- .next/static/chunks/app_layout_tsx_c0237562._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__9d126714._.css",
-  "static/chunks/node_modules_fda8109e._.js",
-  "static/chunks/app_provider_tsx_7beb8f7e._.js"
-],
-    source: "dynamic"
-});
--- .next/static/chunks/components_ui_GridGlobe_tsx_a30c09f5._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([typeof document === "object" ? document.currentScript : undefined, {
-
-"[project]/components/ui/GridGlobe.tsx [app-client] (ecmascript, next/dynamic entry)": ((__turbopack_context__) => {
-
-var { g: global, __dirname } = __turbopack_context__;
-{
-__turbopack_context__.n(__turbopack_context__.i("[project]/components/ui/GridGlobe.tsx [app-client] (ecmascript)"));
-}}),
-}]);
